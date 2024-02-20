f7afb52c2e5aba5deed37579c42a51900f7fc009
Juan Carlos Herrera Blandon <desarrollo.java@ada.co>
2024-02-19 20:05:23 -0500

    Se realiza creacion de estructura proyecto

---
+++ src/main/java/com/api/rest/back_computelematic/BackComputelematicApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.back_computelematic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackComputelematicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackComputelematicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/rest/back_computelematic/BackComputelematicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rest.back_computelematic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackComputelematicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
