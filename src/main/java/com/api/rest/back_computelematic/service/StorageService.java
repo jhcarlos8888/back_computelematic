b8af238f3c801bd97e6e88289e7f875887d9c152
Juan Carlos Herrera Blandon <desarrollo.java@ada.co>
2024-02-20 17:48:02 -0500

    Se realiza creacion de metodos para guardar archivos

---
+++ src/main/java/com/api/rest/back_computelematic/service/StorageService.java
@@ -0,0 +1,17 @@
+package com.api.rest.back_computelematic.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface StorageService {
+    void init() throws IOException;
+
+    String store(MultipartFile file);
+
+    Resource loadAsResource(String filename);
+
+
+
+}
